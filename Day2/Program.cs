72c6d8c919c2085fc8172690634c6f8fb5451fc4
eric <eric.botter@gmail.com>
2024-02-04 22:51:10 +0100

    Part Two

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,34 +1,19 @@
-﻿
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 var numberPattern = new Regex("[0-9]+");
-var gamePattern = new Regex($"(?<red>{numberPattern} red)|(?<green>{numberPattern} green)|(?<blue>{numberPattern} blue)");
+var gamePattern =
+    new Regex($"(?<red>{numberPattern} red)|(?<green>{numberPattern} green)|(?<blue>{numberPattern} blue)");
 
 var lines = File.ReadAllLines("input.txt");
 
-var limitsByColor = new Dictionary<string, int>()
-{
-    { "red", 12 }, { "green", 13 }, { "blue", 14 }
-};
+var colors = new List<string> { "red", "green", "blue" };
 
-var sum = 0;
-
-foreach (var line in lines)
-{
-    var split = line.Split(':');
-
-    var index = int.Parse(numberPattern.Match(split[0]).Value);
-    
-    var throws = split[1].Split(';');
-    var selectMany = throws.SelectMany(round => gamePattern.Matches(round));
-
-    var allGroupsInLimits = selectMany
-        .Select(match => match.Groups.Values.First(group => limitsByColor.ContainsKey(group.Name) && group.Success))
-        .All(group => int.Parse(numberPattern.Match(group.Value).Value) <= limitsByColor[group.Name]);
-    if (allGroupsInLimits)
-    {
-        sum += index;
-    }
-}
+var sum = lines.Sum(line => line.Split(':')[1]
+    .Split(';')
+    .SelectMany(round => gamePattern.Matches(round))
+    .Select(match => match.Groups.Values.First(group => colors.Contains(group.Name) && group.Success))
+    .GroupBy(group => group.Name,
+        (_, values) => values.Select(value => int.Parse(numberPattern.Match(value.Value).Value)).Max())
+    .Aggregate(1, (count1, count2) => count1 * count2));
 
 Console.WriteLine(sum);
