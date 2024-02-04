d8988e7d8bdcc37842172eef892c8862726bf591
eric <eric.botter@gmail.com>
2024-02-04 18:59:15 +0100

    Part one

---
+++ Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("input.txt");
+
+var firstLastChars = from line in lines
+    select (first: line.First(char.IsDigit), last: line.Last(char.IsDigit));
+
+var sum = firstLastChars.Select(x => int.Parse(x.first.ToString()) * 10 + int.Parse(x.last.ToString())).Sum();
+
+Console.WriteLine(sum);
