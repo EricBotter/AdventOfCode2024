74243372d3bef6ad23a218eafaccaa85fa5df66e
eric <eric.botter@gmail.com>
2024-02-05 18:42:53 +0100

    Part two

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -5,12 +5,27 @@ var lineRegex = new Regex($"^Card *{numberRegex}: (?<winning>[0-9 ]*)\\|(?<our>[
 
 var lines = File.ReadAllLines("input");
 
-var sum = lines.Select(line => lineRegex.Match(line))
+var list = lines.Select(line => lineRegex.Match(line))
     .Select(match => (winning: match.Groups["winning"].Value, our: match.Groups["our"].Value))
     .Select(tuple => (winning: numberRegex.Matches(tuple.winning).Select(match => match.Value).Select(int.Parse),
         our: numberRegex.Matches(tuple.our).Select(match => match.Value).Select(int.Parse)))
-    .Select(tuple => tuple.winning.Intersect(tuple.our).ToList())
-    .Select(winningNumbers => winningNumbers.Count > 0 ? 1 << (winningNumbers.Count - 1) : 0)
-    .Sum();
+    .Select(tuple => tuple.winning.Intersect(tuple.our).ToList().Count)
+    .ToList();
+
+var stack = Enumerable.Repeat(1, list.Count).ToList();
+
+var sum = 0;
+foreach (var winning in list)
+{
+    var currentCount = stack[0];
+    stack.RemoveAt(0);
+
+    for (var i = 0; i < winning && i < stack.Count; i++)
+    {
+        stack[i] += currentCount;
+    }
+
+    sum += currentCount;
+}
 
 Console.WriteLine(sum);
