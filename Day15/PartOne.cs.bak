b5fabfdebe981fc9524d0c813b2f3c826d6e9191
eric <eric.botter@gmail.com>
2024-02-18 18:27:36 +0100

    Part Two
    first try

---
+++ Day15/PartOne.cs.bak
@@ -0,0 +1,7 @@
+﻿
+var input = File.ReadAllLines("input")[0].Split(',');
+
+var list = input.Select(s => s.Aggregate(0, (i, c) => (i + c) * 17 % 256)).ToList();
+var sum = list.Sum();
+
+Console.WriteLine(sum);
--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,7 +1,44 @@
 ﻿
 var input = File.ReadAllLines("input")[0].Split(',');
 
-var list = input.Select(s => s.Aggregate(0, (i, c) => (i + c) * 17 % 256)).ToList();
-var sum = list.Sum();
+var boxes = Enumerable.Repeat(0, 256).Select(_ => new Box([])).ToList();
 
+foreach (var s in input)
+{
+    if (s.Contains('='))
+    {
+        var split = s.Split('=');
+        var lens = new Lens(split[0], int.Parse(split[1]));
+        var currentBox = boxes[lens.Hash];
+        
+        var indexOfLabel = currentBox.IndexOfLabel(lens.Label);
+        if (indexOfLabel == -1)
+            currentBox.Lenses.Add(lens);
+        else
+            currentBox.Lenses[indexOfLabel] = lens;
+    }
+    else
+    {
+        var lens = new Lens(s.TrimEnd('-'), 0);
+        var currentBox = boxes[lens.Hash];
+        var indexOfLabel = currentBox.IndexOfLabel(lens.Label);
+        if (indexOfLabel != -1)
+            currentBox.Lenses.RemoveAt(indexOfLabel);
+    }
+}
+
+var sum = boxes.Select((box, i) => box.FocalPower(i)).Sum();
 Console.WriteLine(sum);
+
+record Lens(string Label, int FocalLength)
+{
+    public int Hash => Label.Aggregate(0, (i, c) => (i + c) * 17 % 256);
+}
+
+record Box(List<Lens> Lenses)
+{
+    public int IndexOfLabel(string label) =>
+        Lenses.Select((lens, i) => (lens.Label, i)).FirstOrDefault(tuple => tuple.Item1.Equals(label), ("", -1)).Item2;
+
+    public long FocalPower(int index) => Lenses.Select((lens, i) => (index + 1L) * (i + 1) * lens.FocalLength).Sum();
+};
