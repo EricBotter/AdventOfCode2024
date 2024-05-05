b25eda72f3d8a65afeb0b1c2393b6a23cbe92b21
eric <eric.botter@gmail.com>
2024-05-05 15:21:48 +0200

    Removed gotos from Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,45 +1,45 @@
 ﻿
 var lines = File.ReadAllLines("input.txt");
 
+var dict = new Dictionary<string, string>
+{
+    { "one", "1" },
+    { "two", "2" },
+    { "three", "3" },
+    { "four", "4" },
+    { "five", "5" },
+    { "six", "6" },
+    { "seven", "7" },
+    { "eight", "8" },
+    { "nine", "9" }
+};
+
 string NumbersToDigits(string line)
 {
-    var dict = new Dictionary<string, string>
-    {
-        { "one", "1" },
-        { "two", "2" },
-        { "three", "3" },
-        { "four", "4" },
-        { "five", "5" },
-        { "six", "6" },
-        { "seven", "7" },
-        { "eight", "8" },
-        { "nine", "9" }
-    };
-
     for (var i = 0; i < line.Length; i++)
     {
         if (char.IsDigit(line[i]))
             break;
-        foreach (var pair in dict.Where(pair => line[i..].StartsWith(pair.Key)))
-        {
-            line = line[..i] + pair.Value + line[(i + pair.Key.Length)..];
-            goto FirstReplaceDone;
-        }
+        
+        var (key, value) = dict.FirstOrDefault(pair => line[i..].StartsWith(pair.Key));
+        if (key == null) continue;
+        
+        line = line[..i] + value + line[(i + key.Length)..];
+        break;
     }
 
-    FirstReplaceDone:
     for (var i = line.Length - 1; i >= 0; i--)
     {
         if (char.IsDigit(line[i]))
             break;
-        foreach (var pair in dict.Where(pair => line[i..].StartsWith(pair.Key)))
-        {
-            line = line[..i] + pair.Value + line[(i + pair.Key.Length)..];
-            goto SecondReplaceDone;
-        }
+
+        var (key, value) = dict.FirstOrDefault(p => line[i..].StartsWith(p.Key));
+        if (key == null) continue;
+        
+        line = line[..i] + value + line[(i + key.Length)..];
+        break;
     }
 
-    SecondReplaceDone:
     return line;
 }
 
